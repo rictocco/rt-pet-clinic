eea759dc6b94acede81d5d31dc50279e33049eb5
rtocco <ricardo.tocco@gmail.com>
2018-08-10 16:57:51 +0100

    Initial Project Add

---
+++ src/main/java/auyamatech/com/rtpetclinic/RtPetClinicApplication.java
@@ -0,0 +1,12 @@
+package auyamatech.com.rtpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtPetClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/auyamatech/com/rtpetclinic/RtPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package auyamatech.com.rtpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RtPetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
