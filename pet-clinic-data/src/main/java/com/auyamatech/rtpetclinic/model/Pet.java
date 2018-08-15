398c3adfbd8ebea46ea4f95ffa2761cc5205519d
rtocco <ricardo.tocco@gmail.com>
2018-08-15 13:40:28 +0100

    Adding ID property as Long. Created BaseEntity class. Closes #6

--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.auyamatech.rtpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private Owner owner;
     private LocalDate birthDate;
--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.auyamatech.rtpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
