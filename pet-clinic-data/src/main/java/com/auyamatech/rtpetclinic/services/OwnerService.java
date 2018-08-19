f66805196ee915398f23684f83c8b112fac85780
rtocco <ricardo.tocco@gmail.com>
2018-08-19 13:12:28 +0100

    Refactor service interface into a common base interface. Createed CrudService. closes #7

--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/OwnerService.java
@@ -2,14 +2,6 @@ package com.auyamatech.rtpetclinic.services;
 
 import com.auyamatech.rtpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastname(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package com.auyamatech.rtpetclinic.services;
 
 import com.auyamatech.rtpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findByLastname(String lastName);
-
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
