3d11f55e096ee45cd2766ff72d3d703a2e61e7d6
rtocco <ricardo.tocco@gmail.com>
2018-09-09 14:05:45 +0100

    Added speciality, updated vet with speciality relationship. Closes #28

--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Vet.java
@@ -1,4 +1,21 @@
 package com.auyamatech.rtpetclinic.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vet extends Person{
+
+    private Set<Speciality> specialities;
+
+    public Vet() {
+        specialities = new HashSet<>();
+    }
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
