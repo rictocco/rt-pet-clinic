041a74909b5d603ac7d99814608c13e2dd4843fb
rtocco <ricardo.tocco@gmail.com>
2018-09-09 13:17:15 +0100

    Added visit, updated owners with pet relationship. Closes 27

--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/model/Owner.java
@@ -1,4 +1,22 @@
 package com.auyamatech.rtpetclinic.model;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Owner() {
+        pets = new HashSet<>();
+    }
+
+    public Set<Pet> getPets() {
+        return Collections.unmodifiableSet(pets);
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
