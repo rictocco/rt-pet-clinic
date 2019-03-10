1b80c131abed31bfd3f07b15dbf910b077898751
rictocco <ricardo.tocco@gmail.com>
2019-03-10 22:28:20 +0000

    impl find owner close #55

--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.auyamatech.rtpetclinic.repositories;
 import com.auyamatech.rtpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/auyamatech/rtpetclinic/services/OwnerService.java
@@ -2,6 +2,10 @@ package com.auyamatech.rtpetclinic.services;
 
 import com.auyamatech.rtpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastname(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 }
